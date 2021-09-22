d2f0db3f449f99ef152bb33d496a056a1fe83a91
AymenMSalhi <salhiaymenpro@gmail.com>
2021-09-22 12:38:12 +0200

    Mailing done

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -6,11 +6,26 @@ use Illuminate\Http\Request;
 use App\Models\posts;
 use DB;
 
+use Illuminate\Support\Facades\Mail;
+use App\Mail\NotifMail;
+
 class PostController extends Controller
 {
     public function addPost(Request $request) {
+        $website_ident = $request->input('website_ident');
+        $title = $request->input('title');
+        $description = $request->input('description');
+
+        $subscribers = DB::table('subscribers')->where('website_ident', '=', $website_ident)->get();
+
+        foreach ($subscribers as $key => $user) {
+            Mail::to($user->email)->send(new NotifMail($title, $description));
+        }
+
         posts::create($request->all());
 
+
+        //return response()->json($subscribers);
         return response()->json(["message" => "Post added Successfully!"]);
     }
 
---
+++ resources/views/Email/postNotif.blade.php
@@ -0,0 +1,11 @@
+@component('mail::message')
+
+<h1 style="color: #151515; text-align: center; margin-bottom: 50px;">
+    $title
+</h1>
+
+<div style="color: #555; text-align: center;">
+    $description
+</div>
+
+@endcomponent
